b3eb72eb2317b2798302865cb8aa7362b66ca160
An-Fruit <92803192+An-Fruit@users.noreply.github.com>
2023-02-28 22:48:14 -0600

    Add files via upload
    O(nlogn) solution - trying to figure it out

--- print_lis.java
+++ print_lis.java
@@ -10,14 +10,101 @@ import static java.lang.Math.*;
 import static java.util.Arrays.*;
 
 public class print_lis{
+    //helper binary search method
+    static int GetCeilIndex(int nums[], int tail[], int left, int right, int goal)
+    {
 
+        while (right - left > 1) {
+
+            int mid = left + (right - left) / 2;
+            if (nums[tail[mid]] >= goal)
+                right = mid;
+            else
+                left = mid;
+        }
+
+        return right;
+    }
+
+
+    //optimal method using binary search and a series of "previous" indexes"
+    //https://stackoverflow.com/questions/2631726/how-to-determine-the-longest-increasing-subsequence-using-dynamic-programming
+    //comprehensive python solution to look over
+    static int longest_increasing_subsequence(int nums[], int n)
+    {
+        int N = nums.length;
+
+        // Add boundary case, when array n is zero
+        // Depend on smart pointers
+
+        int tailIndices[] = new int[n];
+
+        // Initialized with 0
+        Arrays.fill(tailIndices, 0);
+
+        int prevIndices[] = new int[n];
+
+        // initialized with -1
+        Arrays.fill(prevIndices, -1);
+
+        // it will always point to empty
+        // location
+        int len = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[tailIndices[0]])
+
+                // new smallest value
+                tailIndices[0] = i;
+
+            else if (nums[i] > nums[tailIndices[len - 1]]) {
+
+                // arr[i] wants to extend
+                // largest subsequence
+                prevIndices[i] = tailIndices[len - 1];
+                tailIndices[len++] = i;
+            }
+            else {
+
+                // arr[i] wants to be a potential
+                // condidate of future subsequence
+                // It will replace ceil value in
+                // tailIndices
+                // perform a binary search from
+                int left = -1;
+                int right = len - 1;
+                //int pos = GetCeilIndex(nums, tailIndices, -1, len - 1, nums[i]);
+                while (right - left > 1) {
+                    int mid = left + (right - left) / 2;
+                    if (nums[tailIndices[mid]] >= nums[i])
+                        right = mid;
+                    else
+                        left = mid;
+                }
+                prevIndices[i] = tailIndices[right - 1];
+                tailIndices[right] = i;
+            }
+        }
+
+//        System.out.println("LIS of given input");
+
+        for (int i = tailIndices[len - 1]; i >= 0;
+             i = prevIndices[i])
+            System.out.print(nums[i] + " ");
+
+//        System.out.println();
+
+        return len;
+    }
+
+
+    //non-optimal method, still cannot pass runtime
     public ArrayList<Integer> get_lis(int[] nums){
         ArrayList<Integer>[] subsequences = new ArrayList[nums.length];
         Arrays.fill(subsequences, new ArrayList<>());
         subsequences[0].add(0);
-
-
         for(int i = 1; i < nums.length; i++){
+            subsequences[i] = new ArrayList<>();
             for(int j = 0; j < i; j++){
                 if(nums[j] < nums[i] && subsequences[j].size() + 1 > subsequences[i].size()){
                     subsequences[i] = new ArrayList<Integer>();
