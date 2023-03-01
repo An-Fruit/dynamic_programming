f32e070c5565d8ea50c8b0bbaf3b028880f0303a
An-Fruit <92803192+An-Fruit@users.noreply.github.com>
2023-03-01 09:31:20 -0600

    Add files via upload

--- knapsack.java
+++ knapsack.java
@@ -13,22 +13,17 @@ public class knapsack{
     //knapsack method where capacity is the capacity of the knapsack, weight is the array of weights,
     //and vals is the array representing the values of the items.
     //n is the length of the weight/value array.
-    //must be modified to also return the indices of the values we are selecting.
-    //keep a mirror matrix that contains a list of the elements you have chosen at that point
-    public void knapSack(int capacity, int weight[], int vals[])
+    public void knapSack(int capacity, int[] weight, int[] vals)
     {
         int curitem, weight_remaining, total_items = weight.length;
         //K is a matrix representing your knapsack.
         // rows denote the current item you are at,
         // and the columns representing how much weight there is left.
         int K[][] = new int[total_items + 1][capacity + 1];
-        ArrayList<Integer>[][] mirror = new ArrayList[total_items + 1][capacity + 1];
-
 
         // Builds the table from the bottom up
         for (curitem = 0; curitem <= total_items; curitem++) {
             for (weight_remaining = 0; weight_remaining <= capacity; weight_remaining++) {
-                mirror[curitem][weight_remaining] = new ArrayList<Integer>();
                 //the 0th item does not exist (there has to be an item after all), therefore all of row 0 is filled with 0
                 //When you have 0 weight (capacity) left, you cannot choose any items, therefore all of column 0 is filled with 0
                 if (curitem == 0 || weight_remaining == 0){
@@ -49,52 +44,41 @@ public class knapsack{
                     //pick the option that gives you the most value
                     if(choose > dont_choose){
                         K[curitem][weight_remaining] = choose;
-                        for(int tmp : mirror[curitem - 1][weight_remaining - weight[curitem - 1]]){
-                            mirror[curitem][weight_remaining].add(tmp);
-                        }
-                        mirror[curitem][weight_remaining].add(curitem - 1);
                     }
                     else{
                         K[curitem][weight_remaining] = dont_choose;
-                        for(int tmp : mirror[curitem - 1][weight_remaining]){
-                            mirror[curitem][weight_remaining].add(tmp);
-                        }
                     }
-//                    K[curitem][weight_remaining] = Math.max(choose, dont_choose);
-//                    K[i][w] = Math.max(vals[i - 1] + K[i - 1][w - weight[i - 1]], K[i - 1][w]);
-
                 }
-
                 //do not choose the current item since you are over or at capacity
                 else
                     K[curitem][weight_remaining] = K[curitem - 1][weight_remaining];
             }
         }
 
-        int max = Integer.MIN_VALUE, maxr = 0, maxc = 0;
-
-
-        for(int i = 0; i < K.length; i++){
-            for(int j = 0; j < K[i].length; j++){
-                if(K[i][j] > max){
-                    maxr = i;
-                    maxc = j;
-                    max = K[i][j];
-                }
+        int val = K[total_items][capacity];
+        Stack<Integer>  indexes = new Stack<>();
+        weight_remaining = capacity;
+        curitem = total_items;
+        //goes in reverse to find the items that you "picked" in your knapsack.
+        for(curitem = total_items; curitem > 0 && val > 0; curitem--){
+            //if you didn't choose an item, then skip
+            if(val == K[curitem - 1][weight_remaining]){
+                continue;
             }
+            indexes.push(curitem - 1);
+            val -= vals[curitem - 1];
+            weight_remaining -= weight[curitem - 1];
         }
-        out.println(mirror[maxr][maxc].size());
-        for(int tmp : mirror[maxr][maxc]){
-            System.out.print(tmp + " ");
+        System.out.println(indexes.size());
+        while(!indexes.isEmpty()){
+            System.out.print(indexes.pop() + " ");
         }
-        out.println();
-        //the element of the matrix at your maximum capacity and
-        //return K[total_items][capacity];
+        System.out.println();
     }
-	public void run() throws Exception{
-	
-	    Scanner f = new Scanner(new File("knapsack.dat"));
-	    //Scanner f = new Scanner(System.in);
+    public void run() throws Exception{
+
+        Scanner f = new Scanner(new File("knapsack.dat"));
+        //Scanner f = new Scanner(System.in);
         while(f.hasNext()){
             int C = f.nextInt();
             int n = f.nextInt();
@@ -106,13 +90,12 @@ public class knapsack{
             }
             knapSack(C, weights, vals);
         }
-
         f.close();
-        
-	}
-	public static void main(String[] args) throws Exception{
-	
-	new knapsack().run();
-	
-	}
+
+    }
+    public static void main(String[] args) throws Exception{
+
+        new knapsack().run();
+
+    }
 }
